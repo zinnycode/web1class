fb277ebd150acc784b955c5c210534f07af483ce
zinnycode <zinny01@gmail.com>
2022-07-27 18:54:57 +0200

    i have done the responsiveness of some media screen and nav

--- JS/main.js
+++ JS/main.js
@@ -9,6 +9,30 @@ $('.owl-carousel').owlCarousel({
         1000:{
             items:3,
             nav:false,
+        },
+        600:{
+            items:2,
+            nav:false,
+        },
+        0:{
+            items:1,
+            nav:false,
         }
     }
+})
+
+const menuOpen = document.querySelector('.hamburger')
+const menuClose = document.querySelector('.xclose')
+const menu = document.querySelector('.navlinks')
+
+menuOpen.addEventListener('click', function(){
+    menu.style.top = "7%"
+    menuOpen.style.display = "none"
+    menuClose.style.display = "block"
+})
+
+menuClose.addEventListener('click', function(){
+    menu.style.top = "-7%"
+    menuClose.style.display = "none"
+    menuOpen.style.display = "block"
 })
